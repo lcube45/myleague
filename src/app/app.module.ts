d0478ab1ad7f6b58ea185c8f548e7736b0c19ff2
lcube <info@lcube.fr>
2017-02-21 12:54:11 +0100

    add firebase dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyAiBN988fGODb9y1fWcPNK0KoSbcI2mr9Y",
+    authDomain: "fir-backend-72948.firebaseapp.com",
+    databaseURL: "https://fir-backend-72948.firebaseio.com",
+    storageBucket: "fir-backend-72948.appspot.com",
+    messagingSenderId: "751412570588"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
